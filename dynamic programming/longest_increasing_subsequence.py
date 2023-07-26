cb6c2518d9032edcdeebe9cf26ddca8f215e0676
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-07-26 10:57:54 +0200

    Add files via upload

--- dynamic programming/longest_increasing_subsequence.py
+++ dynamic programming/longest_increasing_subsequence.py
@@ -11,3 +11,36 @@ def lis(tab):
                 result[i] = result[j] + 1
 
     return max(result), parent
+
+#time: O( nlogn)
+def binary_search(arr, val):
+    left_idx = 0
+    right_idx = len(arr) - 1
+
+    while left_idx <= right_idx:
+        mid_idx = (left_idx + right_idx) // 2
+        if val > arr[mid_idx]:
+            left_idx = mid_idx + 1
+        else:
+            right_idx = mid_idx - 1
+
+    return left_idx  # It will never exceed the left side of an array
+
+
+def lis(arr):
+    if len(arr) < 2:
+        return len(arr)
+
+    n = len(arr)
+    last = []
+
+    for i in range(n):
+        j = binary_search(last, arr[i])
+        if j == len(last):
+            last.append(arr[i])
+        else:
+            last[j] = arr[i]
+
+    print(last)
+    return len(last)
+    
